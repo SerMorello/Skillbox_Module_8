4c935b1902d8a925df470c4f9d1064924da61464
SMorello <morellosharp@gmail.com>
2024-02-15 18:24:47 +0300

    lesson3

---
+++ Module_8/Lesson3_CheckingRepetitions_ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿HashSet<int> numbers = new();
+int number;
+
+while (true)
+{
+    Console.WriteLine("Введите число которое хотите добавить.");
+    number = Convert.ToInt32(Console.ReadLine());
+    if (CheckNumber(number))
+    {
+        numbers.Add(number)
+
+            Console.WriteLine("Число успешно сохранено");
+    }
+    else
+    {
+        Console.WriteLine("Число уже вводилось ранее");
+    }
+}
+
+bool CheckNumber(int number)
+{
+    return numbers.Contains(number);
+}
