d5cbaf5a351970559697dbeb24aa690e5b78b192
SMorello <morellosharp@gmail.com>
2024-02-15 19:14:37 +0300

    Lesson3

---
+++ Module_8/Lesson3_HeshSet_ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿HashSet<int> numbers = new();
+int number;
+
+while (true)
+{
+    Console.WriteLine("Введите число которое хотите добавить.");
+    //number = 
+    //bool result =
+    if (int.TryParse(Console.ReadLine(), out number))
+    {
+        if (!CheckNumber(number))
+        {
+            numbers.Add(number);
+
+            Console.WriteLine("Число успешно сохранено");
+        }
+        else
+        {
+            Console.WriteLine("Число уже вводилось ранее");
+        }
+    }
+    else
+    {
+        break;
+    }
+}
+
+bool CheckNumber(int number)
+{
+    return numbers.Contains(number);
+}
